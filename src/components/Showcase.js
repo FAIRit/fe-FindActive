7ae171009b86e03cced96fc401e7d22bbec5311c
Karolina Michalak <karolina0michalak@gmail.com>
2020-02-25 16:43:32 +0100

    adjusted logo

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,12 +1,19 @@
 import React from "react";
 import styles from "../styles/Showcase.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faDumbbell } from "@fortawesome/free-solid-svg-icons";
 
 const Showcase = () => {
   return (
     <div className={styles.container}>
-      <FontAwesomeIcon icon={faBars} size={"3x"} className={styles.coffee} />
+      <div className={styles.logo}>
+        <FontAwesomeIcon
+          icon={faDumbbell}
+          size={"2x"}
+          className={styles.dumbbell}
+        />
+        <span className={styles.logoText}>FindActive</span>
+      </div>
       <div className={styles.showcase}></div>
     </div>
   );
