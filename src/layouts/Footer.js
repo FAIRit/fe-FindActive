9666538d50b4aba330a4fbeff7a40ad1c38bcb1f
Karolina Michalak <karolina0michalak@gmail.com>
2020-02-25 17:47:43 +0100

    some changes

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "../styles/Footer.module.css";
+import Logo from "../components/Logo";
 
-const Footer = () => <div>Footer</div>;
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <Logo />
+      <NavLink to="/terms">Terms and conditions</NavLink>
+      <NavLink to="/privacy">Privacy Policy</NavLink>
+      <NavLink to="/faq">FAQ</NavLink>
+    </div>
+  );
+};
 
 export default Footer;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Showcase from "../components/Showcase";
+import Footer from "../layouts/Footer";
 import List from "../components/List";
-
+import Faq from "../components/Faq";
+import PrivacyPolicy from "../components/PrivacyPolicy";
+import TermsAndConditions from "../components/TermsAndConditions";
 
 const AppRouter = () => (
-  <Switch>
-    <Route path="/" component={Showcase} exact />
-    <Route path="/list" component={List} exact />
-  </Switch>
+    <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Showcase} exact />
+      <Route path="/list" component={List} exact />
+      <Route path="/faq" component={Faq} exact />
+      <Route path="/privacy" component={PrivacyPolicy} exact />
+      <Route path="/terms" component={TermsAndConditions} exact />
+    </Switch>
+    <Footer />
+    </BrowserRouter>
 );
 
 export default AppRouter;
