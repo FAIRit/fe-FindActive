ec5d420a183984f8d6e151228d317e52fd394e7f
Karolina Michalak <karolina0michalak@gmail.com>
2020-02-25 16:28:16 +0100

    added and formatted menu icon

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -6,9 +6,8 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 const Showcase = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.showcase}>
-        <FontAwesomeIcon icon={faBars} className={styles.coffee} />
-      </div>
+      <FontAwesomeIcon icon={faBars} size={"3x"} className={styles.coffee} />
+      <div className={styles.showcase}></div>
     </div>
   );
 };
