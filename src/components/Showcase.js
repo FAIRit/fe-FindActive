a179df8de4cf23baa915794aba5437d638df9985
Karolina Michalak <karolina0michalak@gmail.com>
2020-02-25 16:20:24 +0100

    finally added fontAwesome

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "../styles/Showcase.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Showcase = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.showcase}></div>
+      <div className={styles.showcase}>
+        <FontAwesomeIcon icon={faBars} className={styles.coffee} />
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './layouts/App';
+// import 'font-awesome/css/font-awesome.min.css';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
