42a543ea1c733c04b926fd9f493857c31ae2e641
Karolina Michalak <karolina0michalak@gmail.com>
2020-02-26 10:16:12 +0100

    Installed another fontawesome package and added fb and insta logos to footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -2,20 +2,26 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "../styles/Footer.module.css";
 import Logo from "../components/Logo";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagramSquare,
+  faFacebookSquare
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
       <Logo />
+      <div>FOLLOW US</div>
+      <FontAwesomeIcon icon={faFacebookSquare} size={"2x"} />
+      <FontAwesomeIcon icon={faInstagramSquare} size={"2x"} />
       <div className={styles.links}>
         <NavLink to="/terms" className={styles.link}>
           Terms and conditions
-        </NavLink>
-        {" "}  {" "}
+        </NavLink>{" "}
         <NavLink to="/privacy" className={styles.link}>
           Privacy Policy
-        </NavLink>
-        {" "}  {" "}
+        </NavLink>{" "}
         <NavLink to="/faq" className={styles.link}>
           FAQ
         </NavLink>
