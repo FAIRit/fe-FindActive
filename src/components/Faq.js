15dc235dc0d35e1fa9410943db0c754b3c92c46e
Karolina Michalak <karolina0michalak@gmail.com>
2020-02-27 12:56:41 +0100

    Styled FAQ and changed questions

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,5 +1,41 @@
 import React from "react";
+import Faq from "react-faq-component";
 
-const Faq = () => <div>Faq</div>;
+const data = {
+  title: "FAQ",
+  rows: [
+    {
+      title: "What is FindActive?",
+      content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. `
+    },
+    {
+      title: "How can I find sport objects near me?",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci."
+    },
+    {
+      title: "Is FindActive account free?",
+      content: `Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus congue lacus eget neque. Phasellus ornare, ante vitae consectetuer consequat, purus sapien ultricies dolor, et mollis pede metus eget nisi. Praesent sodales velit quis augue. Cras suscipit, urna at aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat. `
+    },
+    {
+      title: "Which sport objects are the best?",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    }
+  ]
+};
 
-export default Faq;
+const styles = {
+  titleTextColor: "rgb(12, 35, 76)",
+  rowTitleColor: "rgb(12, 35, 76)",
+  rowContentColor: 'black'
+};
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Faq data={data} styles={styles} />
+      </div>
+    );
+  }
+}
