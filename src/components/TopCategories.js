98cbe91028fd6f5269019fa40dfd5e3645ceb97f
Karolina Michalak <karolina0michalak@gmail.com>
2020-03-01 10:46:34 +0100

    Changed photos in TopCategories section and changed color in MainInfo section

--- src/components/TopCategories.js
+++ src/components/TopCategories.js
@@ -1,8 +1,10 @@
 import React from "react";
 import styles from "../styles/TopCategories.module.css";
-import pole from "../img/pole3.png";
-import yoga from "../img/yog.png";
-import gym from "../img/gym0.jpg";
+import pole from "../img/polepole.png";
+import yoga from "../img/yogayoga.png";
+import gym from "../img/gymgym.png";
+import fight from "../img/fight.png";
+
 
 
 const TopCategories = () => {
@@ -12,7 +14,8 @@ const TopCategories = () => {
       <div className={styles.categoriesContainer}>
         <div className={styles.categorie}><img src={gym} className={styles.categoriePhoto} /><span>Gym</span></div>
         <div className={styles.categorie}><img src={yoga} className={styles.categoriePhoto} /><span>Yoga</span></div>
-        <div className={styles.categorie}><img src={pole} className={styles.categoriePhoto} /><span>Pole</span></div>
+        <div className={styles.categorie}><img src={fight} className={styles.categoriePhoto} /><span>Martial arts</span></div>
+        <div className={styles.categorie}><img src={pole} className={styles.categoriePhoto} /><span>Pole dance</span></div>
       </div>
     </div>
   );
