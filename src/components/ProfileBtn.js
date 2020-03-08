2ac667cbbc56c582839dd4627d7f8a411cd30449
Karolina Michalak <karolina0michalak@gmail.com>
2020-03-08 23:21:14 +0100

    Created FavoritesIcon component and added to navbar

--- src/components/ProfileBtn.js
+++ src/components/ProfileBtn.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Grow from '@material-ui/core/Grow';
-import Paper from '@material-ui/core/Paper';
-import Popper from '@material-ui/core/Popper';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import { makeStyles } from "@material-ui/core/styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser as farFaUser } from "@fortawesome/free-regular-svg-icons";
-import styles from '../styles/ProfileBtn.module.css';
-import firebase from '../firebase/firebase'
+import styles from "../styles/ProfileBtn.module.css";
+import firebase from "../firebase/firebase";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
+    display: "flex"
   },
   paper: {
-    marginRight: theme.spacing(2),
-  },
+    marginRight: theme.spacing(2)
+  }
 }));
 
 export default function MenuListComposition() {
@@ -39,7 +39,7 @@ export default function MenuListComposition() {
   };
 
   function handleListKeyDown(event) {
-    if (event.key === 'Tab') {
+    if (event.key === "Tab") {
       event.preventDefault();
       setOpen(false);
     }
@@ -62,33 +62,43 @@ export default function MenuListComposition() {
     });
   };
 
-
   return (
     <div className={classes.root}>
       <div>
         <Button
           ref={anchorRef}
-          aria-controls={open ? 'menu-list-grow' : undefined}
+          aria-controls={open ? "menu-list-grow" : undefined}
           aria-haspopup="true"
           onClick={handleToggle}
         >
           <FontAwesomeIcon
-                icon={farFaUser}
-                size={"2x"}
-                className={styles.profileIcon}
-            
-              />
+            icon={farFaUser}
+            size={"2x"}
+            className={styles.profileIcon}
+          />
         </Button>
-        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          transition
+          disablePortal
+        >
           {({ TransitionProps, placement }) => (
             <Grow
               {...TransitionProps}
-              style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+              style={{
+                transformOrigin:
+                  placement === "bottom" ? "center top" : "center bottom"
+              }}
             >
               <Paper>
                 <ClickAwayListener onClickAway={handleClose}>
-                  <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
-                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                  <MenuList
+                    autoFocusItem={open}
+                    id="menu-list-grow"
+                    onKeyDown={handleListKeyDown}
+                  >
                     <MenuItem onClick={handleClose}>My account</MenuItem>
                     <MenuItem onClick={signOut}>Logout</MenuItem>
                   </MenuList>
--- src/layouts/LoggedNavbar.js
+++ src/layouts/LoggedNavbar.js
@@ -1,31 +1,19 @@
 import React, { useState } from "react";
 import Logo from "../components/Logo";
 import styles from "../styles/Navbar.module.css";
-import firebase from "../firebase/firebase";
-import MenuList from "@material-ui/core/MenuList";
-
-import { faHeart as farFaHeart } from "@fortawesome/free-regular-svg-icons";
-import MenuListComposition from '../components/ProfileBtn'
+import FavoritesBtn from "../components/FavoritesBtn";
+import MenuListComposition from "../components/ProfileBtn";
 
 class LoggedNavbar extends React.Component {
-
   render() {
-
-
-
     return (
       <div>
         <div className={styles.navbarContainer}>
           <div className={styles.navbar}>
             <Logo />
             <div className={styles.navLinks}>
-             
-              {/* <FontAwesomeIcon
-                icon={farFaHeart}
-                size={"2x"}
-                className={styles.profileIcon}
-              /> */}
-              <MenuListComposition/>
+              <MenuListComposition />
+              <FavoritesBtn />
             </div>
           </div>
         </div>
