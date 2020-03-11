62c84018d53ced79981ad7726ee21208fc9c5e44
Karolina Michalak <karolina0michalak@gmail.com>
2020-03-11 18:17:43 +0100

    Trying to upload photo

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -2,16 +2,18 @@ import React, { useState } from "react";
 import firebase from "../firebase/firebase";
 import Navbar from "../layouts/Navbar";
 import LoggedNavbar from "../layouts/LoggedNavbar";
-import styles from '../styles/UserProfile.module.css';
-import userPlaceholder from '../img/user.jpg'
+import styles from "../styles/UserProfile.module.css";
+import userPlaceholder from "../img/user.jpg";
 
 
-const UserProifile = () => {
+const UserProfile = () => {
+  const auth = firebase.auth();
   const user = firebase.auth().currentUser;
-  const storage = firebase.storage()
+  const storage = firebase.storage();
   const [isLoggedIn, setIsLoggedIn] = useState("");
 
-  const auth = firebase.auth();
+
+
   auth.onAuthStateChanged(user => {
     if (user) {
       setIsLoggedIn(true);
@@ -19,34 +21,45 @@ const UserProifile = () => {
       setIsLoggedIn(false);
     }
   });
- 
-  const [profilePicture, updateProfilePicture] = useState(userPlaceholder)
 
- 
+  var [profilePicture, updateProfilePicture] = useState(userPlaceholder);
 
+updateProfilePicture = e => {
+    if (e.target.files[0]){
+      user
+      .updateProfile({
+        imageURL: e.target.files[0]
+        })
+      }
+    }
 
-
-
-
-  return (
-    <div className={styles.userProfile}>
-      {isLoggedIn ? <LoggedNavbar /> : <Navbar />}
-      {isLoggedIn ? (
-        <div>
-          <div>Witaj, {user.displayName}!</div>
-          <img src={profilePicture} className={styles.profileImg} alt="profile image"/>
+    return (
+      <div className={styles.userProfile}>
+        {isLoggedIn ? <LoggedNavbar /> : <Navbar />}
+        {isLoggedIn ? (
           <div>
-            <form>
-          <label for="file">Zmień zdjęcie</label><input type="file"  accept="image/*"  name="file" id="file" />
-          </form>
-         
+            <div>Witaj, {user.displayName}!</div>
+            <img
+              src={profilePicture}
+              className={styles.profileImg}
+              alt="profile image"
+            />
+            <div>
+              <label for="file">Zmień zdjęcie</label>
+              <input
+                onChange={updateProfilePicture}
+                type="file"
+                accept="image/*"
+                name="file"
+                id="file"
+              />
+            </div>
           </div>
-        </div>
-      ) : (
-        ""
-      )}
-    </div>
-  );
-};
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  };
 
-export default UserProifile;
+export default UserProfile;
