0d1051db6973fcc892f3440544919bf258cd6c25
Karolina Michalak <karolina0michalak@gmail.com>
2020-03-31 13:08:32 +0200

    Adding product to firebase

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,9 +1,120 @@
-import React from 'react'
+import React from "react";
+import LoggedNavbar from "../layouts/LoggedNavbar";
+import SearchBar from "./SearchBar";
+import styles from "../styles/AddProductForm.module.css";
+import { Formik } from "formik";
+import firebase from "../firebase/firebase";
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, "Wpisz co najmniej 3 znaki")
+    .max(50, "Za długa nazwa")
+    .required("Pole wymagane"),
+  location: Yup.string()
+    .min(3, "Wpisz co najmniej 3 znaki")
+    .max(20, "Za długa nazwa")
+    .required("Pole wymagane"),
+  type: Yup.string()
+    .min(3, "Wpisz co najmniej 3 znaki")
+    .max(20, "Za długa nazwa")
+    .required("Pole wymagane"),
+  description: Yup.string()
+    .min(3, "Wpisz co najmniej 3 znaki")
+    .max(500, "Za długi opis")
+    .required("Pole wymagane")
+});
 
 const AddProduct = () => {
-    return(
-        <div>dodaj produkt</div>
-    )
-}
+  return (
+    <div className={styles.addProductPage}>
+      <LoggedNavbar />
+      <SearchBar />
+      <div className={styles.formContainer}>
+        <span className={styles.formTitle}>Dodaj obiekt sportowy do listy</span>
+        <Formik
+          initialValues={{ name: "", location: "", type: "", description: "" }}
+          validationSchema={SignupSchema}
+          onSubmit={values => {
+            const name = values.name;
+            const location = values.location;
+            const type = values.type;
+            const description = values.description;
+            firebase
+              .database()
+              .ref("/clubs")
+              .push({
+                name,
+                location,
+                type,
+                description
+              });
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting
+          }) => (
+            <form onSubmit={handleSubmit} className={styles.addProductForm}>
+              <label htmlFor="name">Nazwa: </label>
+           
+              <input
+                type="text"
+                name="name"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.name}
+              />
+                 {errors.name && touched.name ? (
+                <div className={styles.error}>{errors.name}</div>
+              ) : null}
+             
+              <input
+                type="text"
+                name="location"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.location}
+              />
+               {errors.location && touched.location ? (
+                <div className={styles.error}>{errors.location}</div>
+              ) : null}
+             
+              <input
+                type="text"
+                name="type"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.type}
+              />
+               {errors.type && touched.type ? (
+                <div className={styles.error}>{errors.type}</div>
+              ) : null}
+              
+              <textarea
+                name="description"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.description}
+              />
+              {errors.description && touched.description ? (
+                <div className={styles.error}>{errors.description}</div>
+              ) : null}
 
-export default AddProduct
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+};
+
+export default AddProduct;
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import firebase from "../firebase/firebase";
 import styles from "../styles/UserProfile.module.css";
 import "firebase/storage";
@@ -14,11 +14,8 @@ const UserProfile = () => {
   const storage = firebase.storage();
   const isLoggedIn = useAuth();
 
-  const allInputs = {
-    imgUrl: ""
-  };
   const [imageAsFile, setImageAsFile] = useState("");
-  const [imageAsUrl, setImageAsUrl] = useState(allInputs);
+  const [imageAsUrl, setImageAsUrl] = useState("");
 
   const handleImageAsFile = e => {
     const image = e.target.files[0];
@@ -61,6 +58,8 @@ const UserProfile = () => {
     );
   };
 
+  console.log(imageAsUrl);
+
   return (
     <div className={styles.userProfile}>
       {" "}
@@ -101,3 +100,117 @@ const UserProfile = () => {
 };
 
 export default UserProfile;
+
+// class UserProfile extends React.Component {
+
+// // user = firebase.auth().currentUser;
+
+//   state = {
+//     image: null,
+//     url: ''
+//   }
+
+//   componentDidMount(){
+//     this.checkProfilePicture()
+//   }
+
+//   handleChange = e => {
+//     if(e.target.files[0]){
+//       const image = e.target.files[0]
+//       this.setState({
+//         image
+//       }, () => {
+//         this.handleUpload()
+//         this.setState({
+//           image: null
+//         })
+//       })
+//     }
+//   }
+
+//   handleUpload = () => {
+//     const storage = firebase.storage();
+
+//     const {image} = this.state
+//     if(image){
+//       const uploadTask = storage.ref(`/images/${image.name}`).put(image)
+//       uploadTask.on('state_changed',
+//       (snapshot) => {
+//         console.log(snapshot)
+//       }, error => {
+//         console.log(error)
+//       }, () => {
+//         storage.ref('images').child(image.name).getDownloadURL().then(url => {
+//           console.log(url)
+//           this.setState({url})
+//           this.updateProfilePicture(url)
+
+//         })
+//       })
+//     }
+//   }
+
+//   updateProfilePicture = (url) => {
+//     const currentUser = firebase.auth().currentUser
+//     const id = currentUser.uid
+//     firebase.database().ref(`/users/${id}/profilePicture`).set(url)
+// }
+
+// checkProfilePicture = async () => {
+//   const currentUser = firebase.auth().currentUser
+//   if(currentUser){
+//   const id = currentUser.uid
+//   const dataSnapshot = await firebase.database().ref(`/users/${id}/profilePicture`).once('value')
+//   const profilePictureUrl = dataSnapshot.val()
+//   if (profilePictureUrl) {
+//       this.setState({
+//           url: profilePictureUrl
+//       })
+//   }
+// }
+// }
+
+//   render() {
+//    const user = firebase.auth().currentUser;
+
+//     return (
+//       <div className={styles.userProfile}>
+//         {" "}
+//         {user ? <LoggedNavbar /> : <Navbar />}
+//         <SearchBar />
+//         {user ? (
+//           <div>
+//             <div> Witaj, {user.displayName}! </div>
+//             <img
+//               src={
+//                 this.state.url ||
+//                 "https://semantic-ui.com/images/wireframe/image.png"
+//               }
+//               className={styles.profileImg}
+//               alt="user profile"
+//             />
+//             <div>
+//               <form>
+//                 <label htmlFor="file"> Zmień zdjęcie </label>{" "}
+//                 <input
+//                   type="file"
+//                   name="file"
+//                   id="file"
+//                   accept="image/*"
+//                   onChange={this.handleChange}
+//                 />{" "}
+//                 <button>zmień zdjęcie</button>{" "}
+//               </form>{" "}
+//             </div>{" "}
+//             <UpdatePassword />
+//             <RemoveAccount />
+//           </div>
+//         ) : (
+//           ""
+//         )}{" "}
+//       </div>
+//     );
+//   }
+// }
+
+// export default UserProfile;
