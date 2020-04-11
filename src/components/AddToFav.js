867bcc6c687ea72b8d84642397539c2b1dccbe51
Karolina Michalak <karolina0michalak@gmail.com>
2020-04-11 10:14:54 +0200

    Created functionality of adding products to favorites

--- src/components/AddToFav.js
+++ src/components/AddToFav.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import { Icon } from "semantic-ui-react";
 import styles from "../styles/Product.module.css";
 
-const AddToFav = () => {
-  const [isFav, setIsFav] = useState(false);
+const AddToFav = props => {
 
   return (
     <Icon
-      onClick={() => setIsFav(!isFav)}
-      name={isFav ? "heart" : "heart outline"}
+      onClick={props.onClick}
+    //   name={isFav ? "heart" : "heart outline"}
+    name="heart"
       className={styles.favIcon}
       size="large"
     />
--- src/components/FavPage.js
+++ src/components/FavPage.js
@@ -1,18 +1,104 @@
-import React from "react";
-import { useAuth } from "../hooks/useAuth";
+import React, { Component } from "react";
+import firebase from "../firebase/firebase";
 import LoggedNavbar from "../layouts/LoggedNavbar";
 import Navbar from "../layouts/Navbar";
-import styles from '../styles/FavPage.module.css'
+import styles from "../styles/FavPage.module.css";
+import { displayClubs, stopClubs } from "../services/ClubService";
+import AddToFav from './AddToFav';
+import { addToFav}from '../services/AddToFavService';
+import {Link} from 'react-router-dom'
 
-const FavPage = () => {
-  const isLoggedIn = useAuth();
+class FavPage extends Component {
+  state = {
+    isLoggedIn: false,
+    favorites: {},
+    productList: [],
+  };
 
-  return (
-    <div>
-      {isLoggedIn ? <LoggedNavbar /> : <Navbar />}
-      FavPage
-    </div>
-  );
-};
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          isLoggedIn: true,
+        });
+        firebase
+          .database()
+          .ref("favorites")
+          .child(user.uid)
+          .on("value", (el) => {
+            this.setState({
+              favorites: el.val() || {},
+            });
+          });
+      }
+    });
+
+    displayClubs(productList => {
+        this.setState({
+            productList
+        })
+    })
+  }
+
+  componentWillUnmount() {
+    stopClubs();
+  }
+
+
+
+
+
+
+  loop() {
+    const { productList, favorites } = this.state;
+    const arrayOfFavorites = Object.keys(favorites)
+    const output = []
+    productList.filter(item => {
+        arrayOfFavorites.forEach(element => {
+            if (element === item.id) {
+                output.push(item)
+                return output
+            } else { return }
+        })
+    })
+    if (output == undefined || output == 0) {
+        return ['Lista ulubionych jest pusta']
+    }
+    return output.map((product, index) => {
+        return (
+            <div>
+                <Link to={`/product/${product.id}`}  >
+                 <div>{product.name}</div>
+                 <div>{product.location}</div>
+                </Link>
+                <div className={styles.like}>
+                    <AddToFav id={product.id} isFavorites={this.state.favorites[product.id]}
+                        onClick={() => {
+                            addToFav(product.id, firebase.auth().currentUser)
+                        }
+                        } />
+                </div>
+            </div>
+        )
+    }
+    )
+}
+
+
+
+
+
+
+
+
+  render() {
+    return (
+      <div>
+        {this.state.isLoggedIn ? <LoggedNavbar /> : <Navbar />}
+        {this.loop()}
+      </div>
+    );
+  }
+}
 
 export default FavPage;
