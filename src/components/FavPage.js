d9dc572a3965c22824388fbce3fddf88431b5567
Karolina Michalak <karolina0michalak@gmail.com>
2020-04-12 15:01:27 +0200

    More styling changes

--- src/components/FavPage.js
+++ src/components/FavPage.js
@@ -8,12 +8,13 @@ import AddToFav from "./AddToFav";
 import { addToFav } from "../services/AddToFavService";
 import { Link } from "react-router-dom";
 import { Card, Image } from "semantic-ui-react";
+import Pagination from './Pagination'
 
 class FavPage extends Component {
   state = {
     isLoggedIn: false,
     favorites: {},
-    productList: [],
+    productList: []
   };
 
   componentDidMount() {
