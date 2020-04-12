b10e868da7fc1193301ab14c0e82d48d52b83171
Karolina Michalak <karolina0michalak@gmail.com>
2020-04-12 09:31:39 +0200

    Added prev and next button to pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+
+const Paginationn = ({ postsPerPage, totalPosts, paginate }) => {
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
@@ -19,6 +20,7 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
               style={{ color: "white" }}
             >
               {number}
+
             </div>
           </li>
         ))}
@@ -27,4 +29,4 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
   );
 };
 
-export default Pagination;
+export default Paginationn;
--- src/components/list/ProductListPage.js
+++ src/components/list/ProductListPage.js
@@ -9,7 +9,7 @@ import LoggedNavbar from "../../layouts/LoggedNavbar";
 import { useAuth } from "../../hooks/useAuth";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import Pagination from '../Pagination'
+import Paginationn from "../Pagination";
 
 const ProductListPage = () => {
   const isLoggedIn = useAuth();
@@ -17,7 +17,6 @@ const ProductListPage = () => {
   const [clubsFB, setClubsFB] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage] = useState(3);
-
 
   useEffect(() => {
     displayClubs((clubsFB) => {
@@ -29,22 +28,26 @@ const ProductListPage = () => {
     };
   }, []);
 
-
-
-    const list = clubsFB.map((product) => (
-      <div style={{ marginTop: "30px" }}>
-        <div className={styles.listLink} key={product.id}>
-          <Product {...product} src={product.imageUrl}  />
-        </div>
+  const list = clubsFB.map((product) => (
+    <div style={{ marginTop: "30px" }}>
+      <div className={styles.listLink} key={product.id}>
+        <Product {...product} src={product.imageUrl} />
       </div>
-    ))
+    </div>
+  ));
 
-    const indexOfLastPost = currentPage * postsPerPage;
-    const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = list.slice(indexOfFirstPost, indexOfLastPost)
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = list.slice(indexOfFirstPost, indexOfLastPost);
 
   const paginate = (pageNumber) => {
-    setCurrentPage(pageNumber)
+    setCurrentPage(pageNumber);
+  };
+
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(clubsFB.length / postsPerPage); i++) {
+    pageNumbers.push(i);
   }
 
 
@@ -61,12 +64,36 @@ const ProductListPage = () => {
       ) : (
         ""
       )}
-      <div className={styles.list}>
-       {currentPosts}
-      </div>
-      <Pagination postsPerPage={postsPerPage} totalPosts={clubsFB.length} paginate={paginate}/>
+      <div className={styles.list}>{currentPosts}</div>
+      <button
+        onClick={() => {
+          if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+          } else {
+            return null;
+          }
+        }}
+      >
+        prev
+      </button>
+      <Paginationn
+        postsPerPage={postsPerPage}
+        totalPosts={clubsFB.length}
+        paginate={paginate}
+      />
+      <button
+        onClick={() => {
+          if (currentPage < pageNumbers.length) {
+            setCurrentPage(currentPage + 1);
+          } else {
+            return null;
+          }
+        }}
+      >
+        next
+      </button>
     </div>
-  )
+  );
 };
 
 export default ProductListPage;
