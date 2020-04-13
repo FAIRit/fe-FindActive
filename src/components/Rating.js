78489b3467f6dae3a3b0f23c7c0e6937e4c28b01
Karolina Michalak <karolina0michalak@gmail.com>
2020-04-13 22:45:35 +0200

    Trying to display ratings

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,13 +1,54 @@
 import React from "react";
 import { Rating } from "semantic-ui-react";
-import firebase from '../firebase/firebase'
+import firebase from "../firebase/firebase";
 
 class Rat extends React.Component {
   state = {
     disabled: false,
   };
 
-  handleRate = (e, { rating }) => this.setState({ rating, disabled: true });
+  handleRate = (e, { rating }) => {
+    this.setState({
+      rating,
+    });
+    firebase.database().ref(`/clubs/${this.props.id}/rating`).push({ rating });
+  };
+
+  //   displayRating = () => {
+  //     firebase.database().ref(`/clubs/${this.props.id}/rating`).once('value', function(snapshot) {
+  //        return snapshot
+  //     })
+
+  //   }
+
+  displayRating = () => {
+      const arr = []
+ firebase
+      .database()
+      .ref(`/clubs/${this.props.id}/rating`)
+     .on('value', el => {
+         el.forEach(val => {
+             val.forEach(ee => {
+                //  console.log(ee.val())
+                //  arr.push(ee.val())
+                //  console.log(arr)
+                return (ee.val())
+             })
+         })
+     })
+  };
+
+  displayName = () => {
+    const nejm =  firebase.database().ref(`/clubs/${this.props.id}/name`).on('value', el => {
+         return (el.val())
+      })
+      return nejm
+  }
+
+  //   var starCountRef = firebase.database().ref('posts/' + postId + '/starCount');
+  // starCountRef.on('value', function(snapshot) {
+  //   updateStarCount(postElement, snapshot.val());
+  // });
 
   render() {
     return (
@@ -19,8 +60,9 @@ class Rat extends React.Component {
           maxRating={5}
           onRate={this.handleRate}
           disabled={this.state.disabled}
+          onClick={this.displayRating}
         />
-        Ocena: {this.state.rating} {this.props.id}
+        Ocena: {this.displayRating} {this.displayName} {this.props.id}
       </>
     );
   }
